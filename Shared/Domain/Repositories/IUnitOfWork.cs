37385063a61eb1a01bf6860a3cfb957a9cbd0f55
Enzo Paolo Noblecilla Jimenez <142422788+Enzo-codigo@users.noreply.github.com>
2024-06-28 13:20:59 -0500

    Delete Shared directory

--- Shared/Domain/Repositories/IUnitOfWork.cs
+++
@@ -1,6 +0,0 @@
-namespace si730pc2u202119447.API.Shared.Domain.Repositories;
-
-public interface IUnitOfWork
-{
-    Task CompleteAsync();
-}
--- Shared/Infrastructure/Persistence/EFC/Configuration/AppDBContext.cs
+++
@@ -1,9 +0,0 @@
-using EntityFrameworkCore.CreatedUpdatedDate.Extensions;
-using Microsoft.EntityFrameworkCore;
-
-namespace si730pc2u202119447.API.Shared.Infrastructure.Persistence.EFC.Configuration;
-
-public class AppDBContext(DbContextOptions options) : DbContext(options)
-{
-    
-}
